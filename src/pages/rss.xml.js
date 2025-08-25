548cae8ee50aafa16560753d8e0d04182f896e7b
Clément Latzarus <c@ltzr.io>
2025-08-25 10:27:42 +0200

    feat: generate podcast rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,142 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+// noinspection HttpUrlsUsage
 
-export async function GET(context) {
-  const posts = await getCollection('blog');
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/episodes/${post.id}/`,
-    })),
+import xml2js from 'xml2js';
+import dayjs from 'dayjs';
+import { marked } from 'marked';
+
+import { getCollection } from 'astro:content';
+
+const podcastConfig = {
+  name: 'Les Contes du fond du jardin',
+  description: 'Petites histoires pour oreilles grandes ouvertes',
+  link: 'https://les-contes-du-fond-du-jardin.fr',
+  explicit: false,
+  owner: 'Clément Latzarus',
+  email: 'contact@les-contes-du-fond-du-jardin.fr',
+  category: 'Kids & Family',
+  subcategory: 'Stories for Kids',
+  language: 'fr',
+};
+
+let episodes = await getCollection('blog');
+episodes.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+
+const lastBuildDate = dayjs().format('ddd, DD MMM YYYY hh:mm:ss ZZ');
+const cover = isFullUrl(podcastConfig.cover)
+  ? podcastConfig.cover
+  : podcastConfig.link + podcastConfig.cover;
+
+// noinspection JSUnusedGlobalSymbols
+export async function GET() {
+  let podcast = {
+    rss: {
+      $: {
+        version: '2.0',
+        'xmlns:itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+        'xmlns:podcast':
+          'https://github.com/Podcastindex-org/podcast-namespace/blob/main/docs/1.0.md',
+        'xmlns:atom': 'http://www.w3.org/2005/Atom',
+        'xmlns:content': 'http://purl.org/rss/1.0/modules/content/',
+      },
+      channel: [
+        {
+          title: podcastConfig.name,
+          description: podcastConfig.description,
+          link: podcastConfig.link,
+          copyright: podcastConfig.copyright,
+          author: podcastConfig.author,
+          generator: 'Astro',
+          lastBuildDate: lastBuildDate,
+          language: podcastConfig.language,
+          'itunes:author': podcastConfig.author,
+          'itunes:image': { $: { href: cover } },
+          'itunes:summary': podcastConfig.description,
+          'itunes:type': 'episodic',
+          'itunes:explicit': podcastConfig.explicit,
+          'itunes:owner': {
+            'itunes:name': podcastConfig.owner,
+            'itunes:email': podcastConfig.email,
+          },
+          'itunes:category': {
+            $: {
+              text: podcastConfig.category,
+            },
+            'itunes:category': {
+              $: {
+                text: podcastConfig.subcategory,
+              },
+            },
+          },
+          image: {
+            link: podcastConfig.link,
+            title: podcastConfig.name,
+            url: cover,
+          },
+          'atom:link': [
+            {
+              $: {
+                href: `${podcastConfig.link}/rss.xml`,
+                rel: 'self',
+                type: 'application/rss+xml',
+              },
+            },
+            {
+              $: {
+                href: `https://pubsubhubbub.appspot.com/`,
+                rel: 'hub',
+                type: 'application/rss+xml',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
+
+  podcast.rss.channel[0].item = episodes.map((episode) => {
+    let item = {
+      title: episode.data.title,
+      description: marked.parse(episode.body),
+      pubDate: dayjs(episode.data.pubDate).format(
+        'ddd, DD MMM YYYY hh:mm:ss ZZ',
+      ),
+      link: `${podcastConfig.link}/episode/${episode.id}/`,
+      guid: `${podcastConfig.link}/episode/${episode.id}/`,
+      'itunes:explicit': false,
+      enclosure: {
+        $: {
+          url: isFullUrl(episode.data.audioUrl)
+            ? episode.data.audioUrl
+            : podcastConfig.link + episode.data.audioUrl,
+          length: episode.data.sizeInBytes,
+          type: 'audio/mpeg',
+        },
+      },
+      'itunes:duration': episode.data.durationInSeconds,
+    };
+    const cover_url = episode.data.cover
+      ? episode.data.cover
+      : podcastConfig.cover;
+    item['itunes:image'] = {
+      $: {
+        href: isFullUrl(cover_url) ? cover_url : podcastConfig.link + cover_url,
+      },
+    };
+    return item;
   });
+
+  let builder = new xml2js.Builder({ cdata: true });
+  let xml = builder.buildObject(podcast);
+
+  return new Response(xml, {
+    status: 200,
+  });
+}
+
+function isFullUrl(urlString) {
+  try {
+    return Boolean(new URL(urlString));
+  } catch (e) {
+    return false;
+  }
 }
