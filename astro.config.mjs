baf3211ee7c5e9dd820460ad358cadc8c33ce92b
Clément Latzarus <c@ltzr.io>
2025-09-19 15:04:57 +0200

    fix: set correct base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://les-contes-du-fond-du-jardin.fr',
   integrations: [mdx(), sitemap()],
 });
